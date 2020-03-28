2109406ae5ccdceeb01818cb0407de9dd53d95bc
csantor <christodoulossantorinaios@gmail.com>
2020-03-29 00:34:29 +0200

    Angular HttpClient

--- src/app/dishdetail/dishdetail.component.ts
+++ src/app/dishdetail/dishdetail.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit, Inject } from '@angular/core';
 import { Dish } from '../shared/dish';
 import { DishService } from '../services/dish.service';
 import { Params, ActivatedRoute } from '@angular/router';
@@ -13,12 +13,13 @@ import { Location } from '@angular/common';
 export class DishdetailComponent implements OnInit {
   dish: Dish;
   constructor(private dishservice: DishService,
+    @Inject('BaseURL') public BaseURL,
     private route: ActivatedRoute,
-    private location: Location) { }
+    private location: Location,) { }
   ngOnInit() {
     const id = +this.route.snapshot.params['id'];
     this.dishservice.getDish(id).subscribe(dish => this.dish = dish);
- }
+  }
   goBack(): void {
     this.location.back();
   }
--- src/app/services/dish.service.ts
+++ src/app/services/dish.service.ts
@@ -2,17 +2,21 @@ import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { delay } from 'rxjs/operators';
 import { of } from 'rxjs';
+import { HttpClient } from '@angular/common/http';
+import { baseURL } from '../shared/baseurl';
 
 import { Dish } from '../shared/dish';
 import { DISHES } from '../shared/dishes';
 
 @Injectable()
 export class DishService {
- constructor() { }
- getDishes(): Observable<Dish[]> {
-   return of(DISHES).pipe(delay(2000));
- }
- getDish(id: number): Observable<Dish> {
-   return of(DISHES.filter((dish) => (dish.id === id))[0]).pipe(delay(2000));
- }
+  constructor(private http: HttpClient) { }
+  getDishes(): Observable<Dish[]> {
+    // return of(DISHES).pipe(delay(2000));
+    return this.http.get<Dish[]>(baseURL + 'dishes');
+  }
+  getDish(id: number): Observable<Dish> {
+    // return of(DISHES.filter((dish) => (dish.id === id))[0]).pipe(delay(2000));
+    return this.http.get<Dish>(baseURL + 'dishes/' + id);
+  }
 }
