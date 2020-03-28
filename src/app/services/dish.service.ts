deb9a9c591f12ce4cff98cb2920c39c2fc991fad
csantor <christodoulossantorinaios@gmail.com>
2020-03-28 13:04:18 +0200

    Angular SPA

--- src/app/services/dish.service.ts
+++ src/app/services/dish.service.ts
@@ -11,7 +11,7 @@ export class DishService {
     return DISHES;
   }
 
-  // getDish(id: number): Dish {
-  //   return DISHES.filter((dish) => (dish.id === id))[0];
-  // }
+  getDish(id: number): Dish {
+    return DISHES.filter((dish) => (dish.id === id))[0];
+  }
 }
