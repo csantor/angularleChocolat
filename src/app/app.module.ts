74eb22a4c1b3a872e5cb21c8ef91cd494060ed5f
csantor <christodoulossantorinaios@gmail.com>
2020-03-29 22:58:49 +0300

    Creating data- Forms

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,6 +2,8 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing/app-routing.module';
 import { HttpClientModule } from '@angular/common/http';
+import { FormsModule } from '@angular/forms';
+import { ReactiveFormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { MenuComponent } from './menu/menu.component';
@@ -29,12 +31,14 @@ import { FeedbackService } from './services/feedback.service';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    HttpClientModule
+    HttpClientModule,
+    FormsModule,
+    ReactiveFormsModule
   ],
   providers: [DishService,
     LeaderService,
     FeedbackService,
-    {provide: 'BaseURL', useValue: baseURL}],
+    { provide: 'BaseURL', useValue: baseURL }],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
